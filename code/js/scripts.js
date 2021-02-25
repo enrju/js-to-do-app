5abf6f6fd577fc74c239fb6c04ae9e3fc4998786
enrju <enrju@enrju.pl>
2021-02-25 14:19:42 +0100

    feat: add variable page and function getPage()

--- code/js/scripts.js
+++ code/js/scripts.js
@@ -1,5 +1,22 @@
-function main(){
+function getPage(){
+    const inpNew = document.querySelector("input.new");
+    const inpFind = document.querySelector("input.find");
+    const btnAdd = document.querySelector("button.add");
+    const btnDelete = document.querySelector("button.delete");
+    const h1 = document.querySelector("h1");
+    const ul = document.querySelector("ul");
 
+    return {
+        inpNew,
+        inpFind, 
+        btnAdd, 
+        btnDelete, 
+        h1, 
+        ul};
+}
+function main(){
+    let page = getPage();
+    
 }
 
 main();
